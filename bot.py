4aff16eca52d9d4771e51eb84bfe03ef1268acb3
Tobias Markus <tobias_markus@gmx.net>
2021-03-31 01:12:06 +0200

    Initial commit
    Implemented
    - get_rating_hist
    - get_pgn commands

---
+++ bot.py
@@ -0,0 +1,122 @@
+import json
+import urllib
+from datetime import datetime
+import os
+
+import wget
+import berserk
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from discord import File
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+discord_token = os.getenv('DISCORD_TOKEN')
+lichess_token = os.getenv('LICHESS_TOKEN')
+bot = commands.Bot(command_prefix='!')
+
+session = berserk.TokenSession(lichess_token)
+client = berserk.Client(session=session)
+
+help_str = """
+Gets pgn from lichess
+
+Syntax: !get_pgn <user> <perf_type> <*args>
+
+Examples:
+# All rapid games from 1.1.2021 - 2.2.2021 played as white
+!get_pgn tbsmrks rapid since=1.1.2021 until=2.2.2021 color=white
+
+# All blitz games played ever
+!get_pgn tbsmrks blitz
+
+# All bullet games played as black
+!get_pgn tbsmrks bullet color=black
+"""
+
+
+@bot.command(name='get_pgn', help=help_str)
+async def get_pgn(ctx, user: str, perf_type: str, *args):
+    download_str = f"https://lichess.org/games/export/{user}?perfType={perf_type}"
+    out_file_name = f"{user}_{perf_type}"
+
+    for arg in args:
+        if arg.startswith("until=") or arg.startswith("since="):
+            argument, date = arg.split("=")
+            date_unix = int(datetime.strptime(date, '%d.%m.%Y').timestamp() * 1000)
+            query = "=".join([argument, str(date_unix)])
+        else:
+            query = arg
+        download_str += f"&{query}"
+        file_name_arg = arg.replace('.', '_').replace('=', '_')
+
+        out_file_name += f"_{file_name_arg}"
+
+    out_file_name += ".pgn"
+
+    try:
+        wget.download(download_str, out=out_file_name)
+        await ctx.send(file=File(out_file_name))
+        os.remove(out_file_name)
+    except urllib.error.HTTPError:
+        await ctx.send("Sorry something went wrong maybe a typo? 404 not found!")
+
+help_str = """
+Gets rating history from lichess as excel
+
+Example:
+# export DrNykterstein bullet rating
+!get_rating_hist DrNykterstein bullet
+"""
+
+
+@bot.command(name='get_rating_hist', help=help_str)
+async def get_rating_hist(ctx, user: str, perf_type: str):
+    perf_dict = {
+        "bullet": 0,
+        "blitz": 1,
+        "rapid": 2,
+        "classical": 3,
+        "correspondance": 4,
+        "chess960": 5,
+        "king_of_the_hill": 6,
+        "three_check": 7,
+        "antichess": 8,
+        "atomic": 9,
+        "horde": 10,
+        "racing_kings": 11,
+        "crazy_house": 12,
+        "puzzles": 13,
+        "ultrabullet": 14
+    }
+    # Get the rating History
+    rating_hist = json.loads(client.users.get_rating_history(username=user))
+    # build a rating list with a time string: Tuple[str, int]
+    rating_list = [(f"{rating[0]}-{rating[1] + 1}-{rating[2]}", rating[3]) for rating in
+                   rating_hist[perf_dict[perf_type]]['points']]
+    x = [rating[0] for rating in rating_list]
+    y = [rating[1] for rating in rating_list]
+
+    # create pandas timestamps
+    timestamps = pd.to_datetime(x)
+
+    # create a pandas data series resample it with a daily interval and interpolate the data in a linear fashion
+    ts = pd.Series(y, index=timestamps)
+    ts = ts.resample('D').mean()
+    ts = ts.interpolate(method='time')
+    # plot the data
+    ts.plot(linestyle='dashed', linewidth=1)
+    plt.title(f"{user}'s {perf_type} ratings")
+    out_file_name = f"{user}_{perf_type}.png"
+    plt.savefig(out_file_name)
+    await ctx.send(file=File(out_file_name))
+    os.remove(out_file_name)
+    out_file_name = f"{user}_{perf_type}.xlsx"
+    ts.to_excel(out_file_name)
+    await ctx.send(file=File(out_file_name))
+    os.remove(out_file_name)
+    plt.clf()
+
+bot.run(discord_token)
